2c2a360ce593246befecaa1742c71d9857736fc4
Augusto Omena <augusto.omena.n@gmail.com>
2024-04-03 17:46:25 -0300

    Commenting on the different ways to use the selector

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   // selector: '[app-servers]',
-  selector: '.app-servers',
+  // selector: '.app-servers',
+  selector: 'app-server',
   template: `
     <app-server></app-server>
     <app-server></app-server>`,

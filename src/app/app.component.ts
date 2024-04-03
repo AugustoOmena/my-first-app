3d0abe788e0be119ae5749455bd76d2cee2cd84e
Augusto Omena <augusto.omena.n@gmail.com>
2024-04-03 17:05:38 -0300

    Working with Component Styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.less'
+  //styleUrl: './app.component.less'
+  styles: [`
+    h3 {
+      color: blue;
+    }
+  `]
 })
 export class AppComponent {
-  name = 'Max';
 }
